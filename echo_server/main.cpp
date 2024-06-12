077360b6ea87fb970bf1f01203d21a0cb6c94b66
Dmytro Ochkas <dmytro.ochkas@ansys.com>
2024-06-12 11:41:15 +0200

    Update echo request:
    - Add boost library instead of zlib (managed to compile on MinGW without url module)
    
    - Write a simple multithreading app that prints messages from 2 threads periodically

--- echo_server/main.cpp
+++ echo_server/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <boost/thread.hpp>
 
-#include <zlib.h>
+class TestCallable {
+public:
+  void operator()() {
+    long long i {0};
+    while(true) {
+      std::cout << "Hello from thread " << boost::this_thread::get_id() << ", " << i << " times" << std::endl;
+      boost::this_thread::sleep_for(boost::chrono::milliseconds(2000));
+      i++;
+    }
+  }
+};
 
 int main()
 {
   try
   {
-    std::cout << "First line" << std::endl;
-    std::cout << "Second line" << std::endl;
+    const TestCallable task1{};
+    const TestCallable task2{};
+
+    boost::thread t1(task1);
+    boost::this_thread::sleep_for(boost::chrono::milliseconds(1000));
+    boost::thread t2(task2);
+
+    t1.join();
+    t2.join();
   }
   catch (std::exception& e)
   {
